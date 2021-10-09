fb5a09fcd0b867030bc0c5fd1da93cb7e1f6e74a
Qvelent <48782273+Qvelent@users.noreply.github.com>
2021-10-09 18:11:35 +0300

    Initial commit

---
+++ Assets/Script/CrushDetector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class CrushDetector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.5f;
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Ground")
+        {
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+
+    void ReloadScene()
+   {
+        SceneManager.LoadScene(0);
+   }
+}
